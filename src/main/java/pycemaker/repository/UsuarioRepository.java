8dde2edb717beb613e906bb8e062bfc41aae546b
PedroCruzADS <56441411+PedroCruzADS@users.noreply.github.com>
2022-04-06 10:26:00 -0300

    Alterar repositório

--- src/main/java/pycemaker/repository/UsuarioRepository.java
+++ src/main/java/pycemaker/repository/UsuarioRepository.java
@@ -1,8 +1,12 @@
 package pycemaker.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import pycemaker.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByEmail(String email);
+import java.util.List;
+
+public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
 }
